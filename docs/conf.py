872f256f60300f93c591126c7169f0cd6e5352a1
Bear <94769633+BillSousa@users.noreply.github.com>
2025-09-22 19:57:28 -0400

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'bill_sousa_resume'
+copyright = 'Bill Sousa'
+author = 'Bill Sousa'
+release = 0.0
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'myst_parser'
+]
+
+source_suffix = {'.md': 'markdown'}
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'   #   'alabaster' # 
+html_static_path = ['_static']
+html_context = {
+   # ...
+   "default_mode": "light"
+}
