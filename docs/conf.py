7d968d39e3dce5d04713c970be5ce62e995c9fb3
Bear <94769633+BillSousa@users.noreply.github.com>
2025-09-23 10:08:28 -0400

    Update conf.py 25_09_23_10_08_00

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ source_suffix = {'.md': 'markdown'}
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'   #   'alabaster' # 
+html_theme = 'pydata_sphinx_theme'   #   'alabaster' # 
 html_static_path = ['_static']
 html_context = {
    # ...
