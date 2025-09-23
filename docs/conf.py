c5269f311570047eb95ec6c2f86efa63fb076f3d
Bear <94769633+BillSousa@users.noreply.github.com>
2025-09-23 10:19:56 -0400

    Update conf.py 25_09_23_10_19_00

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'bill_sousa_resume'
 copyright = 'Bill Sousa'
 author = 'Bill Sousa'
-release = 0.0
+release = '0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
