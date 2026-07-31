175221e439d53c5297d4a25175957e8c06d0690f
Bill Sousa <WKSJR99@msn.com>
2026-07-31 07:37:52 -0400

    26_07_31_07_38_00 add html_meta to conf.py for GSC verification

--- docs/conf.py
+++ docs/conf.py
@@ -38,3 +38,8 @@ html_context = {
    # ...
    "default_mode": "light"
 }
+html_meta = {
+    "google-site-verification": "Vfm-HV9ibdA4ubmnyo7bO3KJ4LgmB48Lxp634CmYAPI",
+    "description": "Bill Sousa resume."
+}
+html_baseurl = "https://bill-sousa-resume.readthedocs.io/en/stable/"
